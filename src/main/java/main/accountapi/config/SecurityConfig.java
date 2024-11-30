a9ce9ed8cfc42c490e0dddebb7011a7ff4fcf90f
highsiverr <ye90202@gmail.com>
2024-11-30 15:26:20 +0900

    주석 변경

--- src/main/java/main/accountapi/config/SecurityConfig.java
+++ src/main/java/main/accountapi/config/SecurityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig { // 비밀번호 암호화를 위해 사용 비밀번호는 처음부터 암호화를 해야함
 
     @Bean
     public PasswordEncoder passwordEncoder() {
--- src/main/java/main/accountapi/model/dto/AccountResponse.java
+++ src/main/java/main/accountapi/model/dto/AccountResponse.java
@@ -2,6 +2,6 @@ package main.accountapi.model.dto;
 
 import main.accountapi.model.UserStatus;
 
-//회원가입 응답
+//회원 응답
 public record AccountResponse(long id, String ids, String email, UserStatus status) {}
 
